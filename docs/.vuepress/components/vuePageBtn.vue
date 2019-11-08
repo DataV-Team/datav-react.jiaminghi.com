8630a95fec8da1fcfbe84866c9270aa4f9a79d54
JiaMing <743192023@qq.com>
2019-11-08 16:40:29 +0800

    add new cmp to auto turn vue version page

---
+++ docs/.vuepress/components/vuePageBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <span class="vue-page-btn">
+    （<span class="green" @click="turnToVuePage">Vue版<OutboundLink /></span>）
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'ReactPageBtn',
+  methods: {
+    turnToVuePage () {
+      const { pathname } = location
+
+      window.open(`http://datav.jiaminghi.com${pathname}`)
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.vue-page-btn .green {
+  color: #3eaf7c;
+  cursor: pointer;
+  font-weight: 500;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+}
+</style>
